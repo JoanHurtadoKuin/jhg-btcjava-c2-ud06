a672d858d5fd37fb5425dd604c210b3c4602567a
diablillo99lp <joan.hurtado.garcia@gmail.com>
2022-04-13 11:34:44 +0200

    Ex10Acabado

--- TA06/src/EX10/Ex10App.java
+++ TA06/src/EX10/Ex10App.java
@@ -10,9 +10,60 @@ public class Ex10App {
 		//Scanner
 		Scanner sc = new Scanner(System.in);
 		
+		//Variables
+		int arrayNumeros[];
+		int tamaño;
+		int min;
+		int max;
+		int numero;
+		boolean salir;
 		
+		//Programa
+		System.out.println("Introduce el tamaño de la array");
+		tamaño = sc.nextInt();
 		
+		arrayNumeros = new int[tamaño];
 		
+		//Introduce el rango de numeros
+		System.out.println("Minimo:");
+		min = sc.nextInt();
+		System.out.println("Maximo:");
+		max = sc.nextInt();
+		
+		for (int i = 0; i < arrayNumeros.length; i++) {
+			do {
+				salir = false;
+				numero = numeroAleatorio(min, max);
+				if(calcularNumeroPrimo(numero)) {
+					arrayNumeros[i] = numero;
+					salir = true;
+					System.out.println(arrayNumeros[i]);
+				}
+			} while (!salir);
+		
+		}
+	}
+	
+	private static boolean calcularNumeroPrimo(int num) {
+		
+		//Bucle inicializado a 2, porque el 1 sera siempre divisible
+		//Hasta num-1, porque nuestro numero sera siempre divisible entre el mismo
+		for (int i = 2; i < num; i++) {
+			if(num % i == 0) { //Si un numero es divisor, retornamos false
+				return false;
+			}
+		}
+		
+		//Si acaba el bucle sin encontrar ningun divisor, retornamos true
+		return true;
 	}
 
+	private static int numeroAleatorio(int min, int max) {
+		int numeroAleatorio;
+		
+		//Utilizando esta formula con Math.random le indicamos el abanico de numeros
+        numeroAleatorio = (int) Math.floor(Math.random() * (max - min + 1) + min);
+				
+		return numeroAleatorio;
+	}
 }
